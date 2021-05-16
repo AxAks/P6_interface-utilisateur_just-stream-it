67bf62d669e0ac9203491207ed5b0878642c3932
Aksel <axel.akonde@gmail.com>
2021-05-17 00:21:38 +0200

    permiers tests HTML, JS, AJAX, FETCH recuperer des infos de l'API

--- static/just_stream_it.js
+++ static/just_stream_it.js
@@ -1,14 +1,20 @@
 /* JS Fetch , simpler, native, compatible all browsers
 * Getting data from OCMovies Titles and handling errors
 */
-function fetchAllTitles(url){
-    response = fetch('http://localhost:8000/api/v1/titles')
+async function fetchAllTitles(url){
+
+    let response = await fetch('http://localhost:8000/api/v1/titles/1892')
+    console.log(response.status)
+    console.log(response.statusText)
+    let data = await response.text()  //json(), blob(), formData() and arrayBuffer() selon le type de data
+    console.log(data)
+};
+/*
     .then(response => {
         // gestion des données
     })
     .catch(error => {
         // gestion des erreurs
     })
-    console.log(response.status);
-    console.log(response.statusText);
 };
+*/
