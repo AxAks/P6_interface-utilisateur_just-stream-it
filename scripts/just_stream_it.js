b6a6e2c09928e9f9a16dc6b596eb7c9657812f77
Aksel <axel.akonde@gmail.com>
2021-05-18 15:50:40 +0200

    petites avancées javascript

--- scripts/just_stream_it.js
+++ scripts/just_stream_it.js
@@ -22,31 +22,33 @@ async function fetchAllTitles(url){
 async function fetchTopRatedTitle(url){
 
     let response = await fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score')
-    console.log(response.status)
-    console.log(response.statusText)
+    console.log("Statut page 1: " + response.status + " " + response.statusText)
     var data = await response.json() //json(), blob(), formData() and arrayBuffer() selon le type de data
     console.log(data)
     var results = await data["results"]
     console.log(results)
     var bestMovie = await results[0]
     console.log(bestMovie)
-    /*
-    console.log(getVal);
-    */
 }
 
 /*
     .then(data => {
-        var keys = Object.keys(data['imdb_score']);
-        var firstKey = keys[0];
-        var getVal = obj['imdb_score'][firstKey];
-        console.log(getVal);
-    },) 
+    })
 }
 
-/*
     .catch(error => {
         // gestion des erreurs
     })
 };
 */
+
+async function fetchTopRatedTitles(url){
+    let response = await fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score')
+    console.log("Statut page 1: " + response.status + " " + response.statusText)
+    var data = await response.json() //json(), blob(), formData() and arrayBuffer() selon le type de data
+    console.log(data)
+    var results = await data["results"]
+    console.log(results)
+    var bestMovie = await results[0,1,2,3,4]
+    console.log(bestMovie)
+}
