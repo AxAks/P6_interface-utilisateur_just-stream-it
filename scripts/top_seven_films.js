e99e23d97e492f545f2ad7de266ed4ee2b34df62
Axel <axel.akonde@cinego.net>
2021-05-24 14:41:15 +0200

    Recup top seven OK mais moche, debut recup by genre en  cours

--- scripts/top_seven_films.js
+++ scripts/top_seven_films.js
@@ -2,7 +2,7 @@
 async function fetchBestTwentyFilms(page_url){
     let allData = []; 
     let page_nb = 1;
-    let max_pages = 5; // on s'arrete à 4 apges ici mais pas clair on peut changer le nombre de pages ici (5 resultats par page)
+    let max_pages = 3; // on s'arrete à 2 apges ici mais pas clair on peut changer le nombre de pages ici (5 resultats par page)
 
     while(page_nb < max_pages) {
         page_url = `http://localhost:8000/api/v1/titles/?page=${page_nb}&sort_by=-imdb_score` //on peut changer le filtre ici
@@ -18,15 +18,15 @@ async function fetchBestTwentyFilms(page_url){
 
 // recuperation des infos des 7 meilleurs films
 async function fetchTopSevenFilmInfos(url){
-    let allData = await fetchBestTwentyFilms(); // on recupere les films les mieux notés sur 4 pages (20 films)
-    let films = [];
-    while (films.length < 7){
-        for (film of allData) {
-            films.push(film); // pb j'ai encore 20 films ici !
-        };
-    }
-        console.table(films);
-    return films;
+    let pagesFilms = await fetchBestTwentyFilms(); // on recupere les films les mieux notés sur 4 pages (20 films)
+    let firstSevenfilms = [];
+    i = 0
+    while (i < 7) {
+        firstSevenfilms.push(pagesFilms[i]); // récupération des 7 premiers films seulement
+        i++; 
+    };
+    console.table(firstSevenfilms);
+    return firstSevenfilms;
 };
 
 var yearStart = 2000;
