6b79420b8c39673586a7fc240926803fcaed2cc6
Axel <axel.akonde@cinego.net>
2021-06-11 18:16:03 +0200

    Tops7 n'affichent que 7 films

--- scripts/tops_films.js
+++ scripts/tops_films.js
@@ -6,7 +6,6 @@ const BEST_DRAMA = "http://localhost:8000/api/v1/titles/?page=1&page_size=10&gen
 const BEST_FAMILY = "http://localhost:8000/api/v1/titles/?page=1&page_size=10&genre=family&sort_by=-imdb_score&sort_by=-votes"
 
 
-
 // à garder !!
 async function fetchFilmsBasicInfos(page_url) {
     let response =  await fetch(page_url);
@@ -17,45 +16,45 @@ async function fetchFilmsBasicInfos(page_url) {
 }
 
 // exemple à suivre !!handleTopFilm
-async function handleTopFilm(page_url){
-    const films = await fetchFilmsBasicInfos(page_url);
+async function handleTopFilm(){
+    const films = await fetchFilmsBasicInfos(BEST_OF_ALL);
     const topfilm = document.querySelector(".topfilm");
     topfilm.innerHTML = `<img src="${films[0].image_url}" onclick="showFilmInfos(${films[0].id})">`;
 };
 
-async function handleFilmsforTopTen(page_url){
-    const films = await fetchFilmsBasicInfos(page_url);
+async function handleTopFilms(){
+    const films = await fetchFilmsBasicInfos(BEST_OF_ALL);
     films.shift();
     films.splice(7, 3);
     const carrousel = document.querySelector(".bestfilms > .carrousel");
-    films.forEach((film_infos) => {   // à ajouter : .filter(0, 7, 8, 9 ) à enlever ici
+    films.forEach((film_infos) => {
         carrousel.innerHTML+=`<img src="${film_infos.image_url}" onclick="showFilmInfos(${film_infos.id})">`;
     });
 }
 
-async function handleFilmsforTopTenAction(){
+async function handleTopAction(){
     const films = await fetchFilmsBasicInfos(BEST_ACTION);
     films.splice(7, 3)
     const carrousel = document.querySelector(".bestaction > .carrousel");
-    films.forEach((film_infos) => { // à ajouter : .filter(7, 8, 9 ) à enlever ici
+    films.forEach((film_infos) => {
         carrousel.innerHTML+=`<img src="${film_infos.image_url}" onclick="showFilmInfos(${film_infos.id})">`;
     });
 }
 
-async function handleFilmsforTopTenFamily(){
+async function handleTopFamily(){
     const films = await fetchFilmsBasicInfos(BEST_FAMILY);
     films.splice(7, 3)
     const carrousel = document.querySelector(".bestfamily > .carrousel");
-    films.forEach((film_infos) => {  // à ajouter : .filter(7, 8, 9 ) à enlever ici
+    films.forEach((film_infos) => {
         carrousel.innerHTML+=`<img src="${film_infos.image_url}" onclick="showFilmInfos(${film_infos.id})">`;
     });
 }
 
-async function handleFilmsforTopTenDrama() {
+async function handleTopDrama() {
     const films = await fetchFilmsBasicInfos(BEST_DRAMA);
     films.splice(7, 3)
     const carrousel = document.querySelector(".bestdrama > .carrousel");
-    films.forEach((film_infos) => {   // à ajouter : .filter(7, 8, 9 ) à enlever ici
+    films.forEach((film_infos) => {
         carrousel.innerHTML+=`<img src="${film_infos.image_url}" onclick="showFilmInfos(${film_infos.id})">`;
     });
 }
@@ -67,10 +66,10 @@ async function showFilmInfos(film_id)  {
 
 // Chargement  au démarrage, à simplifier ?
 document.addEventListener('DOMContentLoaded', async () => {
-    handleTopFilm(BEST_OF_ALL);
-    handleFilmsforTopTen(BEST_OF_ALL);
-    handleFilmsforTopTenAction();
-    handleFilmsforTopTenFamily();
-    handleFilmsforTopTenDrama();
+    handleTopFilm();
+    handleTopFilms();
+    handleTopAction();
+    handleTopFamily();
+    handleTopDrama();
 })
 
