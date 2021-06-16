63c26ef0c078bdd8e6048fe9527ef2d24cf760ca
Axel <axel.akonde@cinego.net>
2021-06-16 10:13:13 +0200

    recuperation directe de 7 films dans les tops, plus besoin de supprimer des éléments. + commentaires de todo

--- scripts/tops_films.js
+++ scripts/tops_films.js
@@ -1,16 +1,16 @@
 // CONSTANTS
 const MAIN = "http://localhost:8000/api/v1/titles/"
-const BEST_OF_ALL = "http://localhost:8000/api/v1/titles/?page=1&page_size=10&sort_by=-imdb_scores&sort_by=-votes"
-const BEST_ACTION = "http://localhost:8000/api/v1/titles/?page=1&page_size=10&genre=action&sort_by=-imdb_score&sort_by=-votes"
-const BEST_DRAMA = "http://localhost:8000/api/v1/titles/?page=1&page_size=10&genre=drama&sort_by=-imdb_score&sort_by=-votes"
-const BEST_FAMILY = "http://localhost:8000/api/v1/titles/?page=1&page_size=10&genre=family&sort_by=-imdb_score&sort_by=-votes"
+const BEST_OF_ALL = "http://localhost:8000/api/v1/titles/?page=1&page_size=7&sort_by=-imdb_scores&sort_by=-votes"
+const BEST_ACTION = "http://localhost:8000/api/v1/titles/?page=1&page_size=7&genre=action&sort_by=-imdb_score&sort_by=-votes"
+const BEST_DRAMA = "http://localhost:8000/api/v1/titles/?page=1&page_size=7&genre=drama&sort_by=-imdb_score&sort_by=-votes"
+const BEST_FAMILY = "http://localhost:8000/api/v1/titles/?page=1&page_size=7&genre=family&sort_by=-imdb_score&sort_by=-votes"
 
 let topAllFilms;
 let topActionFilms;
 let topFamilyFilms;
 let topDramaFilms;
 
-// à garder !!
+
 async function fetchFilmsBasicInfos(page_url) {
     let response =  await fetch(page_url);
     let data =  await response.json();
@@ -19,6 +19,8 @@ async function fetchFilmsBasicInfos(page_url) {
     });
 }
 
+
+//Gestion du TOP FILM 
 // exemple à suivre !! mais gestion affichage des infos differente..
 async function handleTopFilm(){
     const films = await fetchFilmsBasicInfos(BEST_OF_ALL);
@@ -32,31 +34,22 @@ async function handleTopFilm(){
     };
 };
 
-/*
-// à finir !!!
-async function showTopFilmInfos(film_id) {
-    top_film_infos_section = document.querySelector(".topfilmBasicInfos");
-    required_infos = await getInfos(film_id);
-    top_film_infos_section.innerHTML = required_infos;
-}
-*/
 
+// Gestion des Top Categories
+//essayer de factoriser en handleTops(caterogy) avec variable category et if all : shift() 
 async function handleTopAll(){
     topAllFilms = await fetchFilmsBasicInfos(BEST_OF_ALL);
     topAllFilms.shift();
-    topAllFilms.splice(7, 3);
     handleCarrousel('bestfilms', '');
 };
 
 async function handleTopAction(){
     topActionFilms = await fetchFilmsBasicInfos(BEST_ACTION);
-    topActionFilms.splice(7, 3)
     handleCarrousel('bestaction', '');
 };
 
 async function handleTopFamily(){
     topFamilyFilms = await fetchFilmsBasicInfos(BEST_FAMILY);
-    topFamilyFilms.splice(7, 3)
     handleCarrousel('bestfamily', '');
 };
 
@@ -68,8 +61,30 @@ async function handleTopDrama() {
 
 
 
-
+//Récuperation des infos pour un film
 // à améliorer
+async function getInfos(film_id){
+    let response = await fetch(`http://localhost:8000/api/v1/titles/${film_id}`);
+    let detailed_infos =  await response.json();
+    let required_infos = {
+        'Poster': `<img src="${detailed_infos.image_url}">`,
+        'Title': detailed_infos.original_title,
+        'Directors': detailed_infos.directors,
+        'Actors': detailed_infos.actors,
+        'Genres': detailed_infos.genres,
+        'Countries': detailed_infos.countries,
+        'Release Date': detailed_infos.date_published,
+        'Duration': detailed_infos.duration,
+        'Rated': detailed_infos.rated,
+        'Box Office': `$ ${detailed_infos.worldwide_gross_income}`,
+        'Long Description': detailed_infos.long_description
+    };
+    return required_infos;
+
+};
+
+//Affichage des infos pour un film
+// à améliorer + bouton close foireux
 async function showDetailedInfos(film_id) {
     required_infos = await getInfos(film_id);
 
@@ -89,31 +104,13 @@ async function showDetailedInfos(film_id) {
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
-        };                                             // When the user clicks anywhere outside of the modal, close it
+        };
     };
 }
 
-// à améliorer
-async function getInfos(film_id){
-    let response = await fetch(`http://localhost:8000/api/v1/titles/${film_id}`);
-    let detailed_infos =  await response.json();
-    let required_infos = {
-        'Title': detailed_infos.original_title,
-        'Directors': detailed_infos.directors,
-        'Actors': detailed_infos.actors,
-        'Genres': detailed_infos.genres,
-        'Countries': detailed_infos.countries,
-        'Release Date': detailed_infos.date_published,
-        'Duration': detailed_infos.duration,
-        'Rated': detailed_infos.rated,
-        'Box Office': `$ ${detailed_infos.worldwide_gross_income}`,
-        'Long Description': detailed_infos.long_description
-    };
-    return required_infos;
-
-}
 
 
+// Gestion du Carrousel, à refactoriser
 async function handleCarrousel (category, up_or_down) {
     let films;
     let new_films;
@@ -172,7 +169,7 @@ async function handleCarrousel (category, up_or_down) {
 
 
 
-// Chargement  au démarrage, à simplifier ?
+// Chargement  au démarrage
 document.addEventListener('DOMContentLoaded', async () => {
     handleTopFilm();
     handleTopAll();
